40c7c4405bdbd22712732d594d1ae40b1694ceba
Claudineia Gonçalves <100870046+claudineiaga@users.noreply.github.com>
2023-06-12 15:04:50 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const CONFIGDATA = {
+    titleHeader: "Quer Namorar Comigo?",
+    descriptionHeader:
+      "Se você sair e/ou não responder, significa que você CONCORDA ^^",
+    buttonYes: "Sim",
+    buttonNo: "Não!",
+    titleModar: "Obrigado por Aceitar",
+    descriptionModar:
+      'Na verdade, eu sabia que você aceitaria^^ </br>',
+  };
+  
